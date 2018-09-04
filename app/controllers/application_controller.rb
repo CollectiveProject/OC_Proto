198333ccc2e6bd5e780ceb5faaae4c630206dd24
Nick McD <nickmcdoz@gmail.com>
2018-09-04 03:08:10 -0400

    added user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,8 @@
 class ApplicationController < ActionController::Base
 
-
-def testcode
-  name = "Nick"
-    render html: "Hi there, sexy penis (and face.)"
-end
-
-
-
-
-
-
-
+  def testcode
+    name = "Nick"
+      render html: "Welcome to the pre-launch page of the pre-alpha version of OpenCast"
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get  '/signup',  to: 'users#new'
   get 'pages/Home'
   get 'pages/Blueprints'
   get 'pages/Prototype'
   get 'pages/Contact'
   get 'pages/OC_Coin'
 
+
   Rails.application.routes.draw do
+  get 'users/new'
     root 'application#testcode'
   end
 
