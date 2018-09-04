06118fe470875245877cdc52dac6eb92fd99d3a6
Nick McD <nickmcdoz@gmail.com>
2018-09-04 05:28:49 -0400

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates(:name,  presence: true, length: { maximum: 28 })
+
+  validates(:email, presence: true, length: { maximum: 255 },
+  uniqueness: true)
 end
